6ea7aa510c5e8ae6ff095da48606e70e5f92c104
Davide <daviderappa96@gmail.com>
2026-02-22 19:42:13 +0100

    Integrazione PWA: manifest, service-worker e meta tag iOS

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'biker-manager-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './motor-theme.css',
+    './AuthManager.js',
+    './GarageManager.js',
+    './RouteManager.js',
+    './CostManager.js',
+    './NotificationManager.js',
+    './SettingsManager.js',
+    './GearingManager.js',
+    './DocManager.js',
+    './WeatherManager.js',
+    './ExportManager.js',
+    './ImageManager.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
